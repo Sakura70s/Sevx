6e8862ff4fff18b98610aed1bd290d35fcaa1c83
Sakura70s <Sakura70s@outlook.com>
2022-06-01 09:05:06 +0800

    实现所有模块的 长按删除

--- app/src/main/java/top/sakura70s/sevx/adapters/NovelItemAdapter.java
+++ app/src/main/java/top/sakura70s/sevx/adapters/NovelItemAdapter.java
@@ -9,16 +9,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import top.sakura70s.sevx.R;
 import top.sakura70s.sevx.SevxConsts;
 import top.sakura70s.sevx.activitys.MainActivity;
 import top.sakura70s.sevx.beans.novel.NovelBean;
+import top.sakura70s.sevx.helpers.JsonFrom;
+import top.sakura70s.sevx.helpers.RequestHelper;
 
 public class NovelItemAdapter extends RecyclerView.Adapter<NovelItemAdapter.NovelItemHolder> {
     private List<NovelBean> list;
@@ -45,6 +56,35 @@ public class NovelItemAdapter extends RecyclerView.Adapter<NovelItemAdapter.Nove
             intent.putExtra(SevxConsts.UNAME, uName);
             intent.putExtra(SevxConsts.UPASSWORD, uPassword);
             context.startActivity(intent);
+        });
+
+        holder.itemView.setOnLongClickListener(view -> {
+            AlertDialog dialog = new AlertDialog.Builder(view.getContext())
+                    .setIcon(R.drawable.ic_delete)
+                    .setTitle("删除")
+                    .setMessage("确定要删除吗？")
+                    .setNegativeButton("取消", (dialogInterface, i) -> dialogInterface.dismiss())
+                    .setPositiveButton("确定", (dialogInterface, i) -> {
+                        dialogInterface.dismiss();
+                        // 构建请求体
+                        JSONObject json = new JsonFrom().deleteJson(id, uName, uPassword);
+                        Request request = new RequestHelper().getDeleteRequest(SevxConsts.NOVEL_DELETE, json);
+                        // 执行删除
+                        new OkHttpClient().newCall(request).enqueue(new Callback() {
+                            @Override
+                            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+                            }
+
+                            @Override
+                            public void onResponse(@NonNull Call call, @NonNull Response response) {
+
+                            }
+                        });
+                    }).create();
+            // 显示模态对话框
+            dialog.show();
+            return true;
         });
     }
 
--- app/src/main/java/top/sakura70s/sevx/adapters/video/VideoItemFilmAdapter.java
+++ app/src/main/java/top/sakura70s/sevx/adapters/video/VideoItemFilmAdapter.java
@@ -9,16 +9,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import top.sakura70s.sevx.SevxConsts;
 import top.sakura70s.sevx.activitys.MainActivity;
 import top.sakura70s.sevx.R;
 import top.sakura70s.sevx.beans.film.VideoFilmBean;
+import top.sakura70s.sevx.helpers.JsonFrom;
+import top.sakura70s.sevx.helpers.RequestHelper;
 
 public class VideoItemFilmAdapter extends RecyclerView.Adapter<VideoItemFilmAdapter.VideoItemFilmHolder> {
     // 定义 传进来的 集合
@@ -54,6 +65,35 @@ public class VideoItemFilmAdapter extends RecyclerView.Adapter<VideoItemFilmAdap
             intent.putExtra(SevxConsts.UPASSWORD, uPassword);
             context.startActivity(intent);
 
+        });
+
+        holder.itemView.setOnLongClickListener(view -> {
+            AlertDialog dialog = new AlertDialog.Builder(view.getContext())
+                    .setIcon(R.drawable.ic_delete)
+                    .setTitle("删除")
+                    .setMessage("确定要删除吗？")
+                    .setNegativeButton("取消", (dialogInterface, i) -> dialogInterface.dismiss())
+                    .setPositiveButton("确定", (dialogInterface, i) -> {
+                        dialogInterface.dismiss();
+                        // 构建请求体
+                        JSONObject json = new JsonFrom().deleteJson(id, uName, uPassword);
+                        Request request = new RequestHelper().getDeleteRequest(SevxConsts.VIDEO_FILM_DELETE, json);
+                        // 执行删除
+                        new OkHttpClient().newCall(request).enqueue(new Callback() {
+                            @Override
+                            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+                            }
+
+                            @Override
+                            public void onResponse(@NonNull Call call, @NonNull Response response) {
+
+                            }
+                        });
+                    }).create();
+            // 显示模态对话框
+            dialog.show();
+            return true;
         });
     }
 
